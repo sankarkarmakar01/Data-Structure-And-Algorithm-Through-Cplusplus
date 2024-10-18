ed4c9e7c81bd3399c74d0f23b0212626fa7584be
Sankar Karmakar <162941085+sankarkarmakar01@users.noreply.github.com>
2024-10-18 12:37:10 +0530

    Create Unique_Number_of_Occurrences.cpp

---
+++ All_Practic_Questions/Unique_Number_of_Occurrences.cpp
@@ -0,0 +1,32 @@
+//1207. Unique Number of Occurrences
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        sort(arr.begin(),arr.end());
+        int i = 0;
+        while(i<n)
+        {
+            int count = 1;
+            for(int j=i+1;j<n;j++)
+            {
+                if(arr[i] == arr[j])
+                    count++;
+                else
+                    break;
+            }
+            ans.push_back(count);
+            i = i + count;
+        }
+        n = ans.size();
+        sort(ans.begin(),ans.end());
+        for(int i = 0;i<n-1;i++)
+        {
+            if(ans[i] == ans[i+1])
+                return false;
+        }
+        return true;
+    }
+};
