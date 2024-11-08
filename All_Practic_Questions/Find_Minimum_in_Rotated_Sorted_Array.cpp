07e99764f690e7a8ac8e143df6a1e694a2ef2635
Sankar Karmakar <162941085+sankarkarmakar01@users.noreply.github.com>
2024-11-08 09:28:16 +0530

    Create Find_Minimum_in_Rotated_Sorted_Array.cpp

---
+++ All_Practic_Questions/Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,22 @@
+// LeetCode - 153
+
+public:
+    int findMin(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size()-1;
+        int ans = nums[0];
+        int mid;
+        while(start<=end)
+        {
+            mid = start + (end - start)/2;
+            if(nums[mid] >= nums[0])
+                start = mid + 1;
+            else 
+            {
+                ans = nums[mid];
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
